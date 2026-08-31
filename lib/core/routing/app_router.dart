8739c6dbb4c6788453bf22635b257af9fc8f33d7
Manar-97 <Manar-97@github.com>
2026-08-31 17:51:09 +0300

    added images and svgs folders for assets add flutter_svgs anf flutter_screenutil made a file for app doc added navigation extention added routing folder added colors and text styles added empty login screen added onboarding screen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,24 @@
+import 'package:appointment_app/core/routing/routes.dart';
+import 'package:flutter/material.dart';
+import '../../features/login/login_screen.dart';
+import '../../features/onboarding/ui/onboarding_screen.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      case (Routes.onBoardingScreen):
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+      case (Routes.loginScreen):
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route default for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
